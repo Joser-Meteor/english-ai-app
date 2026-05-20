6f3842c6a1e6879d489015107d89f4b227a697af
Joser <1554620956@qq.com>
2026-05-20 12:03:11 +0800

    修复知识库页面横向溢出被裁剪、删除按钮不可达

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -44,7 +44,7 @@ export default function Layout() {
       </header>
 
       {/* 主内容 */}
-      <main className="flex-1 min-h-0 max-w-4xl mx-auto w-full px-4 pt-4 pb-0 overflow-hidden flex flex-col">
+      <main className="flex-1 min-h-0 max-w-4xl mx-auto w-full px-4 pt-4 pb-0 flex flex-col">
         <Outlet />
       </main>
     </div>
